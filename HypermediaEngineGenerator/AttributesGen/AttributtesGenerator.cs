0e1e824e39bce12e3e1aaef1447da4b1f03a824c
Lucas <luisernestlucas@gmail.com>
2025-05-24 09:09:12 +0100

    feat: Add Enable Attribute

---
+++ HypermediaEngineGenerator/AttributesGen/AttributtesGenerator.cs
@@ -0,0 +1,14 @@
+﻿using HypermediaEngineGenerator.AttributesGen.Attrubutes;
+using Microsoft.CodeAnalysis;
+
+namespace HypermediaEngineGenerator.Attributes
+{
+    internal static class AttributtesGenerator
+    {
+        internal static IncrementalGeneratorInitializationContext AddAttributes(this IncrementalGeneratorInitializationContext context)
+        {
+            HypermediaEnableAttribute.AddAttributeToSource(context);
+            return context;
+        }
+    }
+}
---
+++ HypermediaEngineGenerator/AttributesGen/Attrubutes/HypermediaEnableAttribute.cs
@@ -0,0 +1,29 @@
+﻿using HypermediaEngineGenerator.Helper;
+using Microsoft.CodeAnalysis;
+
+namespace HypermediaEngineGenerator.AttributesGen.Attrubutes
+{
+    internal static class HypermediaEnableAttribute
+    {
+        internal const string FileName = "HypermediaAttribute";
+        private const string Class = @"
+namespace Hypermedia.Attributes
+{
+    [System.AttributeUsage(System.AttributeTargets.Class)]
+    public class HypermediaEnableAttribute : System.Attribute
+    {
+            public HypermediaEnableAttribute(Type dto)
+            {
+                Dto = dto;
+            }
+
+            public Type Dto { get; }
+    }
+}";
+
+        internal static IncrementalGeneratorInitializationContext AddAttributeToSource(this IncrementalGeneratorInitializationContext context)
+        {
+            return context.AddFileToSource(Class, FileName);
+        }
+    }
+}
