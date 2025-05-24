9acd34d97dd3451ec3f2998798d153dee86c3fda
Lucas <luisernestlucas@gmail.com>
2025-05-24 09:08:20 +0100

    feat: Add Extension Helper

---
+++ HypermediaEngineGenerator/Helper/GeneratorExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+namespace HypermediaEngineGenerator.Helper
+{
+    internal static class GeneratorExtensions
+    {
+        private static readonly string s_fileNameConvetion = ".g.cs";
+
+        internal static IncrementalGeneratorInitializationContext AddFileToSource(this IncrementalGeneratorInitializationContext context, string classText, string fileName)
+        {
+            context.RegisterPostInitializationOutput(ctx => ctx.AddSource(
+                                                        string.Concat(fileName, s_fileNameConvetion),
+                                                        GetSourceText(classText)
+                                                        ));
+            return context;
+        }
+
+        private static SourceText GetSourceText(string classText) => SourceText.From(classText, Encoding.UTF8);
+    }
+}
