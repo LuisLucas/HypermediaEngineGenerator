66e9d8a4bf4bff9f202d64e744f8fe27b3749a5b
Lucas <luisernestlucas@gmail.com>
2025-05-25 11:17:14 +0100

    feat: Add IHyperMediaGenerator interface.

---
+++ HypermediaEngineGenerator/InterfacesGen/Interfaces/IHypermediaGenerator.cs
@@ -0,0 +1,28 @@
+﻿using HypermediaEngineGenerator.Helper;
+using Microsoft.CodeAnalysis;
+
+namespace HypermediaEngineGenerator.InterfacesGen.Interfaces
+{
+    internal static class IHypermediaGenerator
+    {
+        private const string FileName = "IHypermediaGenerator";
+        public const string Class = @"
+namespace Hypermedia.Interfaces;
+
+public interface IHypermediaGenerator<T>
+{
+    Resource<T> GenerateLinks(T item, HttpContext httpContext);
+
+    Resource<T> GenerateLinks(T item, Type controller);
+
+    CollectionResource<T> GenerateLinks(IEnumerable<T> items, Type controller);
+
+    PaginatedResource<T> GenerateLinks(IEnumerable<T> items, Type controller, int page, int pageSize, int totalNumberOfRecords);
+}";
+
+        internal static IncrementalGeneratorInitializationContext AddToSource(this IncrementalGeneratorInitializationContext context)
+        {
+            return context.AddFileToSource(Class, FileName);
+        }
+    }
+}
---
+++ HypermediaEngineGenerator/InterfacesGen/InterfacesGenerator.cs
@@ -0,0 +1,14 @@
+﻿using HypermediaEngineGenerator.InterfacesGen.Interfaces;
+using Microsoft.CodeAnalysis;
+
+namespace HypermediaEngineGenerator.InterfacesGen
+{
+    internal static class InterfacesGenerator
+    {
+        internal static IncrementalGeneratorInitializationContext AddAttributes(this IncrementalGeneratorInitializationContext context)
+        {
+            IHypermediaGenerator.AddToSource(context);
+            return context;
+        }
+    }
+}
