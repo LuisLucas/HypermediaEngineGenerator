6ca489f8638a5c8fd1c3c73fca6adaca72988b22
Lucas <luisernestlucas@gmail.com>
2025-05-25 11:16:31 +0100

    feat: add class attributes.

---
+++ HypermediaEngineGenerator/AttributesGen/Attrubutes/HypermediaAttribute.cs
@@ -0,0 +1,34 @@
+﻿using HypermediaEngineGenerator.Helper;
+using Microsoft.CodeAnalysis;
+
+namespace HypermediaEngineGenerator.AttributesGen.Attrubutes
+{
+    internal static class HypermediaAttribute
+    {
+        private const string FileName = "HypermediaAttribute";
+        private const string Class = @"
+namespace Hypermedia.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class HypermediaAttribute : Attribute
+{
+    public string Method { get; }
+    public string Relation { get; }
+    public string Property { get; }
+    public string Controller { get; }
+
+    public HypermediaAttribute(string method, string relation, string property, string controller = """")
+    {
+        Method = method;
+        Relation = relation;
+        Property = property;
+        Controller = controller;
+    }
+}";
+
+        internal static IncrementalGeneratorInitializationContext AddToSource(this IncrementalGeneratorInitializationContext context)
+        {
+            return context.AddFileToSource(Class, FileName);
+        }
+    }
+}
---
+++ HypermediaEngineGenerator/AttributesGen/Attrubutes/HypermediaListAttribute.cs
@@ -0,0 +1,30 @@
+﻿using HypermediaEngineGenerator.Helper;
+using Microsoft.CodeAnalysis;
+
+namespace HypermediaEngineGenerator.AttributesGen.Attrubutes
+{
+    internal static class HypermediaListAttribute
+    {
+        private const string FileName = "HypermediaListAttribute";
+        private const string Class = @"
+namespace Hypermedia.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class HypermediaListAttribute : Attribute
+{
+    public string Method { get; }
+    public string Relation { get; }
+
+    public HypermediaListAttribute(string method, string relation)
+    {
+        Method = method;
+        Relation = relation;
+    }
+}";
+
+        internal static IncrementalGeneratorInitializationContext AddToSource(this IncrementalGeneratorInitializationContext context)
+        {
+            return context.AddFileToSource(Class, FileName);
+        }
+    }
+}
