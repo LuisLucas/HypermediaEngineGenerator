7c0003e158b0a44541753b1f8f69bf173867832d
Lucas <luisernestlucas@gmail.com>
2025-06-01 21:05:55 +0100

    feat: add namedTypeSymbol extension.

---
+++ HypermediaEngineGenerator/Helper/INamedTypeSymbolExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Collections.Generic;
+
+namespace HypermediaEngineGenerator.Helper
+{
+    internal static class INamedTypeSymbolExtensions
+    {
+        internal static string GetFullNamespace(this INamedTypeSymbol symbol)
+        {
+            var namespaces = new Stack<string>();
+            INamespaceSymbol ns = symbol.ContainingNamespace;
+
+            while (!ns.IsGlobalNamespace)
+            {
+                namespaces.Push(ns.Name);
+                ns = ns.ContainingNamespace;
+            }
+
+            return string.Join(".", namespaces);
+        }
+    }
+}
