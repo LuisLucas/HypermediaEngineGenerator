655f92044ea6f5d761af5c7eefb8a0d33e481326
Lucas <luisernestlucas@gmail.com>
2025-06-13 16:37:56 +0100

    feat: add todo comment

--- HypermediaEngineGenerator.Demo/Model/HypermediaEngineGeneratorModel.cs
+++ HypermediaEngineGenerator.Demo/Model/HypermediaEngineGeneratorModel.cs
@@ -6,7 +6,7 @@ namespace HypermediaEngineGenerator.Demo.Model
     [Hypermedia("PUT", "edit", "Id")]
     [Hypermedia("DELETE", "delete", "Id")]
     //[Hypermedia("GET", "related", "Id", "stocks")]
-    [HypermediaList("GET", "self")]
+    [HypermediaList("GET", "self")] // TODO: identifie the parameter name for pagination if we are using the option with pagination on the generated class.
     [HypermediaList("POST", "create")]
     public class HypermediaEngineGeneratorModel
     {
